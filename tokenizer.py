aa67d1cfb5b38062a64bd927629256cbcaa610f7
Adrien Sade <asade@asade-mac2.myfiosgateway.com>
2024-08-12 22:21:16 -0400

    trained tokenizer

--- tokenizer.py
+++ tokenizer.py
@@ -2,7 +2,8 @@
 Define Tokenizer class (base) and the GPT2Tokenizer class wrapper to implement specific behavior of GPT2 Tokenizer.
 """
 
-import re
+import regex as re
+import unicodedata
 
 GPT2_SPLIT_PATTERN = r"""'(?:[sdmt]|ll|ve|re)| ?\p{L}+| ?\p{N}+| ?[^\s\p{L}\p{N}]+|\s+(?!\S)|\s+"""
 GPT4_SPLIT_PATTERN = r"""'(?i:[sdmt]|ll|ve|re)|[^\r\n\p{L}\p{N}]?+\p{L}+|\p{N}{1,3}| ?[^\s\p{L}\p{N}]++[\r\n]*|\s*[\r\n]|\s+(?!\S)|\s+"""
@@ -43,10 +44,10 @@ def clean_control_char(string):
     chars = []
     for char in string:
         if unicodedata.category(char)[0] == 'C':
-            chars.append(f"\\u{ord(ch):04x}")           # unknown utf-8 char
+            chars.append(f"\\u{ord(char):04x}")           # unknown utf-8 char
         else:
             chars.append(char)
-    return chars
+    return "".join(chars)
 
 def render_token(token):
     """
@@ -60,7 +61,7 @@ def render_token(token):
 class Tokenizer:
     """ Base Tokenizer Class """
 
-    def __init__():
+    def __init__(self):
         self.merges = {}
         self.pattern = ""
         self.special_tokens = {}
@@ -97,59 +98,58 @@ class Tokenizer:
         """
 
         with open(file_name+'.model', 'w') as file:
-            file.write(self.__class__.__name__ + '\n')      # name/version of tokenizer
-            file.write(self.pattern + '\n')                 # write the pattern
-            file.write(len(self.special_tokens) + '\n')     # write the nb of special tokens
+            file.write(self.__class__.__name__ + '\n')              # name/version of tokenizer
+            file.write(self.pattern + '\n')                         # write the pattern
+            file.write(str(len(self.special_tokens)) + '\n')        # write the nb of special tokens
             for special, idx in self.special_tokens.items():
-                file.write(special + ' ' + idx + '\n')      # write the special tokens
+                file.write(special + ' ' + str(idx) + '\n')         # write the special tokens
             for idx1, idx2 in self.merges:
-                file.write(idx1 + ' ' + idx2 + '\n')        # write the merges
+                file.write(str(idx1) + ' ' + str(idx2) + '\n')      # write the merges
             
-        
         merges_revert = {idx: pair for pair, idx in self.merges.items()}
-        with open(file_name+'.vocab', 'w') as file:
+        with open(file_name+'.vocab', 'w', encoding='utf-8') as file:
             for idx, token in self.vocab.items():
                 s = render_token(token)
                 if idx in merges_revert:
                     idx0, idx1 = merges_revert[idx]
-                    s0, s1 = render_token(idx0), render_token(idx1)
+                    s0, s1 = render_token(self.vocab[idx0]), render_token(self.vocab[idx1])
                     file.write(f'[{s0}][{s1}] -> [{s}] {idx}\n')
                 else:
                     file.write(f'[{s}] {idx}\n')
 
 
-        def load(self, file_name):
-            """
-            Loads file_name.model and builds vocab, ie load tokenizer.
-            """
+    def load(self, file_name):
+        """
+        Loads file_name.model and builds vocab, ie load tokenizer.
+        """
 
-            assert file_name.endswith(".model")
-            merges = {}
-            special_tokens = {}
-            pattern = ""
-            idx = 256
+        assert file_name.endswith(".model")
+        merges = {}
+        special_tokens = {}
+        pattern = ""
+        idx = 256
 
-            with open(file_name, 'r', encoding='utf-8') as file:
-                name_version = file.readline().strip()
-                assert 'GPT2' in name_version
+        with open(file_name, 'r', encoding='utf-8') as file:
+            name_version = file.readline().strip()
+            assert 'GPT2' in name_version
 
-                pattern = file.readline().strip()
-                num_special_tokens = int(file.readline().strip())
+            pattern = file.readline().strip()
+            num_special_tokens = int(file.readline().strip())
 
-                for  _ in range(num_special_tokens):
-                    special, special_idx = file.readline().strip().split()
-                    special_tokens[special] = int(special_idx)
-                
-                for line in f:
-                    idx1, idx2 = line.split()
-                    idx1, idx2 = int(idx1), int(idx2)
-                    merges[(idx1, idx2)] = idx
-                    idx += 1
+            for  _ in range(num_special_tokens):
+                special, special_idx = file.readline().strip().split()
+                special_tokens[special] = int(special_idx)
             
-            self.merges = merges
-            self.special_tokens = special_tokens
-            self.pattern = pattern
-            self.vocab = self._build_vocab()
+            for line in file:
+                idx1, idx2 = line.split()
+                idx1, idx2 = int(idx1), int(idx2)
+                merges[(idx1, idx2)] = idx
+                idx += 1
+        
+        self.merges = merges
+        self.special_tokens = special_tokens
+        self.pattern = pattern
+        self.vocab = self._build_vocab()
 
 class GPT2Tokenizer(Tokenizer):
     """ GPT2 Tokenizer Class, infant Tokenizer class to reproduce GPT2 """
@@ -177,7 +177,6 @@ class GPT2Tokenizer(Tokenizer):
             stats = {}
             for chunk_ids in text_ids:
                 stats = get_stats(chunk_ids, stats)
-
             max_pair = max(stats, key=stats.get)
             new_id = 256 + i
 
@@ -186,7 +185,7 @@ class GPT2Tokenizer(Tokenizer):
             vocab[new_id] = vocab[max_pair[0]] + vocab[max_pair[1]]
 
             if verbose:
-                print(f"Merge {i+1}/{num_merges}: `{vocab[max_pair[0]]}` (token [{max_pair[0]}]) + `{vocab[max_pair[1]]}` (token [{max_pair[1]}]) --> `{vocab[new_id]}` (token [{new_id}])")
+                print(f"Merge {i+1}/{num_merges}: {vocab[max_pair[0]]} [{max_pair[0]}] + {vocab[max_pair[1]]} [{max_pair[1]}] --> {vocab[new_id]} [{new_id}]")
             
             self.merges = merges
             self.vocab = vocab
@@ -226,7 +225,7 @@ class GPT2Tokenizer(Tokenizer):
         while len(bytes_ids) >= 2:
             stats = get_stats(bytes_ids)
             min_pair = min(stats, key=lambda p: stats.get(p, float('inf')))     # select the pair with lowest merging opportunities
-            if min_pair not in self.merge:
+            if min_pair not in self.merges:
                 break                                                           # stop merging if no more merging opportunities
 
             idx = self.merges[min_pair]
